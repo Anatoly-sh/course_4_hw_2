5fcc0b964f23987b49e92e445ec6b1cecd8ca13d
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-02-26 21:29:13 +0300

    Код разработан

--- youtube.py
+++ youtube.py
@@ -1,7 +1,6 @@
 import os
 import json
 from googleapiclient.discovery import build
-import pprint
 
 """
 Файл .env в корне проекта  — это текстовый файл, содержащий пары “ключ/значение” 
@@ -10,39 +9,56 @@ import pprint
 """
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 # api_key: str = os.getenv('SKYPROAPIKEY')
 
 # создать специальный объект для работы с API
 # youtube = build('youtube', 'v3', developerKey=api_key)
 
-channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'       # вДудь
+channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
+
+
 # channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'       # Редакция
 # channel_id = 'UClMe70teNK5LpBE3Im_JRSQ'  # Знай ТВ
-
 
 # channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
 # print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-class YoutubeAnalitica:
+class YoutubeAnalytic:
+    load_dotenv()
 
     def __init__(self, id_channel, api_key):
         self.api_key = os.getenv(api_key)  # ключ из .env
-        self.id_channel = id_channel
+        self.__id_channel = id_channel
         # специальный объект для работы с API
         self.service = build('youtube', 'v3', developerKey=self.api_key)
+        self.channel_info = self.service.channels().list(id=channel_id, part='snippet,statistics').execute()
+
+        self.title = self.channel_info['items'][0]['snippet']['title']
+        self.description = self.channel_info['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/' + self.__id_channel
+        self.subscriber_count = self.channel_info['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel_info['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel_info['items'][0]['statistics']['viewCount']
+        data = self.title + self.description + self.url + self.subscriber_count + self.video_count + self.view_count
+
+    @classmethod
+    def get_service(cls):
+        service = build('youtube', 'v3', developerKey=os.environ.get('YOUTUBE_KEY'))
+        return service
+
+    def json_file(self, data, filename='channel.json'):
+        """добавляет в json файл информацию о канале, хранящуюся в атрибутах"""
+        with open(filename, 'r') as file:
+            file_data = json.load(file)
+            file_data.append(data)
+            return json.dump(file_data, file, indent=4)
 
     def print_info(self):
         # см. методы build() , channels() , list() , execute()
-        self.info = json.dumps(self.service.channels().list(id=self.id_channel, part='snippet,statistics').execute(),
-                               ensure_ascii=False)
-        return self.info
+        print(json.dumps(self.channel_info, indent=2, ensure_ascii=False))
 
 
-item = YoutubeAnalitica(channel_id, "SKYPROAPIKEY")
+item = YoutubeAnalytic(channel_id, "SKYPROAPIKEY")
 
-# print(item.print_info())
-
-pprint.pprint(item.print_info())
+item.print_info()
