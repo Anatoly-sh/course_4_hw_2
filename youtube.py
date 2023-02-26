29abaf40e300a44e4d0854e7d17ee9b981d36875
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-02-26 20:37:30 +0300

    Учтены замечания по предыдущему ДЗ

--- youtube.py
+++ youtube.py
@@ -2,6 +2,7 @@ import os
 import json
 from googleapiclient.discovery import build
 import pprint
+
 """
 Файл .env в корне проекта  — это текстовый файл, содержащий пары “ключ/значение” 
 всех переменных среды. Необходимо установить библиотеку python-dotenv. load_dotenv() 
@@ -17,28 +18,30 @@ load_dotenv()
 # создать специальный объект для работы с API
 # youtube = build('youtube', 'v3', developerKey=api_key)
 
-# channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'       # вДудь
+channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'       # вДудь
 # channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'       # Редакция
-channel_id = 'UClMe70teNK5LpBE3Im_JRSQ'         # Знай ТВ
+# channel_id = 'UClMe70teNK5LpBE3Im_JRSQ'  # Знай ТВ
+
 
 # channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
 # print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-class Youtube_analitica:
+class YoutubeAnalitica:
 
     def __init__(self, id_channel, api_key):
-        self.api_key = os.getenv(api_key)                       # ключ из .env
+        self.api_key = os.getenv(api_key)  # ключ из .env
         self.id_channel = id_channel
         # специальный объект для работы с API
         self.service = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self):
         # см. методы build() , channels() , list() , execute()
-        self.info = json.dumps(self.service.channels().list(id=self.id_channel, part='snippet,statistics').execute())
+        self.info = json.dumps(self.service.channels().list(id=self.id_channel, part='snippet,statistics').execute(),
+                               ensure_ascii=False)
         return self.info
 
 
-item = Youtube_analitica(channel_id, "SKYPROAPIKEY")
+item = YoutubeAnalitica(channel_id, "SKYPROAPIKEY")
 
 # print(item.print_info())
 
