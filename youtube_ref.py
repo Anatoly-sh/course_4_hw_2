59e9681ca9832030e02139e8ceed5bb556d8557e
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-03-10 20:21:15 +0300

    проверка всех заданий

--- youtube_ref.py
+++ youtube_ref.py
@@ -350,11 +350,11 @@ if __name__ == '__main__':
     # print(video1)
     # print(video2)
     # 5
-    # pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
-    # print(pl.title)
-    # print(pl.url)
-    # duration = pl.total_duration
-    # print(duration)
-    # print(type(duration))
-    # print(duration.total_seconds())
-    # print(pl.show_best_video())
+    pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
+    print(pl.title)
+    print(pl.url)
+    duration = pl.total_duration
+    print(duration)
+    print(type(duration))
+    print(duration.total_seconds())
+    print(pl.show_best_video())
