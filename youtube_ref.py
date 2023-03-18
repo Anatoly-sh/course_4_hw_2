a88553811c932d32bb97982b2b6b39c76b20589b
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-03-18 15:39:53 +0300

    README.md

--- youtube_ref.py
+++ youtube_ref.py
@@ -343,7 +343,7 @@ class YoutubeApiError(Exception):
         return self.message
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
     # 1
     # vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
@@ -370,11 +370,11 @@ if __name__ == '__main__':
     # print(video1)
     # print(video2)
     # 5
-    pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
-    print(pl.title)
-    print(pl.url)
-    duration = pl.total_duration
-    print(duration)
-    print(type(duration))
-    print(duration.total_seconds())
-    print(pl.show_best_video())
+    # pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
+    # print(pl.title)
+    # print(pl.url)
+    # duration = pl.total_duration
+    # print(duration)
+    # print(type(duration))
+    # print(duration.total_seconds())
+    # print(pl.show_best_video())
